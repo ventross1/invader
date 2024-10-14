2930323d0b1a66d3e6b682b91eb6e935744528c5
vašek chvátal <38693443+ventross1@users.noreply.github.com>
2024-10-14 11:16:28 +0200

    Add files via upload

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 var test = Vector2.ZERO + Vector2(27,27)
-var down_speed = 15
+var down_speed = 20
 var screen_size
 var munkey = 1
+var funky = 0
 func _ready() -> void:
 	screen_size = get_viewport_rect().size + Vector2(-10,-10)
 	hide()
@@ -14,17 +15,25 @@ func audio():
 
 func _process(delta: float) -> void:
 	var speed = 30 + Global.killed
-	position += transform.x * speed * delta
+	if funky == 0:
+		position += transform.x * speed * delta
+	if funky == 1:
+		position -= transform.x * speed * delta
 	$AnimatedSprite2D.play("enemy")
 	if Global.start == 1:
 		show()
 	#position = position.clamp(test, screen_size)
-	if global_position.x > 460:
+	if global_position.x > 650:
 		mun()
-	print(speed)
+	if global_position.x < -180:
+		sun()
 	if global_position.y > 480:
 		get_tree().change_scene_to_file("res://start.tscn")
 
 func mun():
-	global_position.x = -10
 	global_position.y = global_position.y + down_speed
+	funky = 1
+
+func sun():
+	global_position.y = global_position.y + down_speed
+	funky = 0
